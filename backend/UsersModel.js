9384e7848801145f18d8d21b1294b204267137ab
elkanio <lukaskania2@gmail.com>
2020-01-14 19:36:37 +0100

    update mongo

--- backend/UsersModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const usersSchema = new Schema({
-    id: Number,
-    name: String,
-})
-
-var User = mongoose.model('users', usersSchema);
